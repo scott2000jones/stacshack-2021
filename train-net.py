c8c93167b03c0dd233c334d4dc9ad90f425210b0
Scott Jones <scott2000jones@gmail.com>
2021-03-13 19:20:09 +0000

    training script takes weights and dataset from command line

--- train-net.py
+++ train-net.py
@@ -1,8 +1,25 @@
+import sys
 from textgenrnn import textgenrnn
 
-nn = textgenrnn('./weights/loverboy.hdf5')
-# if you want to reset the weights to zero --> nn.reset()
-nn.train_from_file('./datasets/loverboy.txt', num_epochs=100)
-# nn.save('./weights/loverboy.hdf5')
-v = nn.generate(return_as_list=True, max_gen_length=300)[0]
-print('train-net> ' + str(v))
+# Script to train neural network on data in given weights file with given training data, specified in command line args (see usage comment)
+if len(sys.argv) != 3:
+    print('Usage:')
+    print('python3 train-net.py <weights-file> <dataset-file>')
+    print('Be careful! The given weights file *will* be overwritten!')
+    exit()
+
+# Open a textgenrnn using the given weights file
+nn = textgenrnn(str(sys.argv[1])) 
+
+# If you want to reset the weights to zero --> nn.reset()
+# But be careful! This will overwrite the weights in the file given above
+
+# Train the nn on given dataset for given epoch number
+# Weights are saved after each epoch, so don't worry about terminating early
+nn.train_from_file(str(sys.argv[2]), num_epochs=100)
+
+# Optional force save --> nn.save('./weights/loverboy.hdf5')
+
+# Example use case of generating a result from a trained network:
+# v = nn.generate(return_as_list=True, max_gen_length=300)[0]
+# print('train-net> ' + str(v))
